2ac87cf36f0216796ffee420997e2109c4cd8028
Max M <meshstyles@users.noreply.github.com>
2020-07-13 12:58:11 +0200

    revamped join/leave role(group)

--- index.js
+++ index.js
@@ -25,40 +25,45 @@ bot.on("message", async message => {
     switch (argssingle[0].toLocaleLowerCase()) {
         case `${botconfig.prefix}help`:
             return message.author.send(guildPersonality.messages.help);
+
         case vote:
             return voting(message);
+
         case leave:
-            return leaver(message);
+            if (argssingle.length <= 1)
+                return;
+
+            var args1 = argssingle[1];
+
+            if (!guildPersonality.roles.includes(args1))
+                return message.reply("this role is not supported");
+            var roler = message.guild.roles.cache.find(r => r.name === args1);
+
+            if (!message.member.roles.cache.has(roler.id))
+                return message.reply("you don't have that role");
+
+            message.member.roles.remove(roler).catch(console.error);
+            return message.reply(`:crab: is gone from \"${args1}\" :crab: `);
+
         case join:
-            return joiner(message);
+            if (argssingle.length <= 1)
+                return;
+
+            var args1 = argssingle[1];
+
+            if (!guildPersonality.roles.includes(args1))
+                return message.reply("this role is not supported");
+            var roler = message.guild.roles.cache.find(r => r.name === args1);
+
+            if (message.member.roles.cache.has(roler.id))
+                return message.reply("you already have that role");
+
+            message.member.roles.add(roler).catch(console.error);
+            return message.reply(` Achievement Get! \" ${args1} \" `);
+
         default:
             return;
     }
-
-    // //this code is less effecient than two seperate methods but easier to maintain
-    // if (argssingle[0] === leave || argssingle[0] === join) {
-    //     var args1 = argssingle[1].toLocaleLowerCase();
-    //     // role => role.name === "ModStaff"
-    //     if (!message.member.roles.has(message.guild.roles.find(role => role.name === `${args1}`))) {
-    //         switch (args1) {
-    //             case "nofreegames":
-    //                 break;
-    //             case "netTest":
-    //                 break;
-    //             case "helfer":
-    //                 break;
-    //             case "lfg":
-    //                 break;
-    //             default:
-    //                 return message.reply("thats not a role please check out my help page https://meshstyles.github.io/unilandiscordbot/");
-    //         }
-    //         if (argssingle[0] === leave) { leaver() }
-    //         if (argssingle[0] === join) { joiner() }
-    //     }
-    // }
-    // if (message.content.startsWith(leave)) {
-    //     var args1 = argssingle[1].toLocaleLowerCase();
-    // }
 
 });
 bot.login(botconfig.token);
@@ -81,16 +86,4 @@ function voting(message) {
             }
         })
     }
-}
-
-function joiner() {
-    var roler = message.guild.roles.find(role => role.name === `${args1} `);
-    message.reply(` Achievement Get! \" ${args1} \" `);
-    return member.addRole(roler).catch(console.error);
-}
-
-function leaver() {
-    var roler = message.guild.roles.find(role => role.name === `${args1}`);
-    message.reply(`:crab: is gone from \"${args1}\" :crab: `);
-    return member.removeRole(roler).catch(console.error);
 }
